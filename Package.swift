14589cd0c9fee5c25912bbd72b9224aa147584d9
Jason Flax <jsflax@gmail.com>
2021-12-16 18:28:39 +0000

    add xcf

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://github.com/jsflax/spm_test/releases/download/1.0.2/Realm.xcframework.zip",
-            checksum: "84e5b552c376182efe5491e1b833c3e198ffb17ad3db2298f2a8b1a19edcd001"),
+            path: "Realm.xcframework"),
         .target(
             name: "RealmSwift",
             dependencies: ["Realm"]),
